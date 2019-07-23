28165ce2775c1a15b73d165c59c8991ae238ded5
Zachary Falkner <zac.falkner@submittable.com>
2019-07-23 15:28:23 -0600

    init

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+const stati = [
+  // 'notScanned',
+  // 'isComplete',
+  'inProgress',
+  'isVirus',
+]
+
+const randomStati = () => {
+  return stati[Math.floor(Math.random() * stati.length)];
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/files', function(req, res) {
+  var files = req.body.files
+  let status = {}
+  files.forEach(guid => {
+    status[guid] = {
+      status: randomStati(),
+      timestamp: 'the time',
+    }
+  })
+  // let status = {...files.map(guid => { 
+  //     return { 
+  //       [guid]: {
+  //         status: randomStati(),
+  //         timestamp: 'the time'
+  //       } 
+  //     } 
+  //   })
+  // }
+  console.log(status)
+  res.send(status)
+})
+
+module.exports = router;
